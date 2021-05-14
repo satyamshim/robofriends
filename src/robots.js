278aea986edaa12a6803c97df2f3e92a978c45e4
satyamshim <56406263+satyamshim@users.noreply.github.com>
2021-05-14 15:50:45 +0530

    I dont know

---
+++ src/robots.js
@@ -0,0 +1,58 @@
+export const robots = [
+  {
+    id: 'Daddy',
+    name: 'Manoj Kumar ',
+    username: 'Daddy',
+    email: '9419255093'
+  },
+  {
+    id: 'Mom',
+    name: 'Rani Devi',
+    username: 'Mom',
+    email: '9796219926'
+  },
+  {
+    id: 'yashika__sharma',
+    name: 'Yashika Sharma',
+    username: 'yashika__sharma',
+    email: '9418966315'
+  },
+  {
+    id: 'KT',
+    name: 'Kavita Agnihotri',
+    username: 'KT',
+    email: '6006341521'
+  },
+  {
+    id: 'aagnihotri_',
+    name: 'Aknush Agnihotri',
+    username: 'aagnihotri_',
+    email: '9149521578'
+  },
+  {
+    id: '_pre_et',
+    name: 'Preeti Agnihotri',
+    username: '_pre_et',
+    email: '9149630992',
+   
+  },
+  {
+    id: 'shimbhat',
+    name: 'Satyam Bhat',
+    username: 'shimbhat',
+    email: '7889898611',
+    
+  },
+  {
+    id: '__harrsh_',
+    name: 'Harsharan Singh Raina',
+    username: '__harrsh_',
+    email: '7006001507',
+  },
+  {
+    id: 'nehamarch2',
+    name: 'Neha Kashyap',
+    username: 'nehamarch2',
+    email: 'Working On It!'
+  }
+];
